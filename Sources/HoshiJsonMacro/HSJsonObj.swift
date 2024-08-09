6148ff4ab77fffa46b838f54784687fe24da3af3
星空凛 <mouguoge@bilibili.com>
2024-08-09 19:05:41 +0800

    类名更改 + equal判断

--- Sources/HoshiJsonMacro/HSJsonObj.swift
+++ Sources/HoshiJsonMacro/HSJsonObj.swift
@@ -1,5 +1,5 @@
 //
-//  HSJsonValue.swift
+//  HSJsonObj.swift
 //
 //
 //  Created by 星空凛 on 2024/8/9.
@@ -7,11 +7,11 @@
 
 import Foundation
 
-public struct HSJsonValue {
+public struct HSJsonObj {
     private var value: Any?
 }
 
-extension HSJsonValue: ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, ExpressibleByStringLiteral, ExpressibleByBooleanLiteral {
+extension HSJsonObj: ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, ExpressibleByStringLiteral, ExpressibleByBooleanLiteral {
     public init(integerLiteral value: Int) {
         self.value = value
     }
@@ -29,7 +29,7 @@ extension HSJsonValue: ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, E
     }
 }
 
-extension HSJsonValue: ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral, ExpressibleByNilLiteral {
+extension HSJsonObj: ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral, ExpressibleByNilLiteral {
     public init(arrayLiteral elements: Any...) {
         self.value = elements
     }
@@ -41,17 +41,33 @@ extension HSJsonValue: ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral
     public init(nilLiteral: ()) { }
 }
 
-extension HSJsonValue: CustomStringConvertible, Equatable {
+extension HSJsonObj: CustomStringConvertible, Equatable {
     public var description: String {
         return "\(value ?? "nil")"
     }
     
-    public static func == (lhs: HSJsonValue, rhs: HSJsonValue) -> Bool {
-        return false // TODO: 补充
+    public static func == (lhs: HSJsonObj, rhs: HSJsonObj) -> Bool {
+        if let l = lhs.value as? Int, let r = rhs.value as? Int {
+            return l == r
+        } else if let l = lhs.value as? Double, let r = rhs.value as? Double {
+            return l == r
+        } else if let l = lhs.value as? String, let r = rhs.value as? String {
+            return l == r
+        } else if let l = lhs.value as? Bool, let r = rhs.value as? Bool {
+            return l == r
+        } else if let l = lhs.value as? [HSJsonObj], let r = rhs.value as? [HSJsonObj] {
+            return l == r
+        } else if let l = lhs.value as? [String:HSJsonObj], let r = rhs.value as? [String:HSJsonObj] {
+            return l == r
+        } else if lhs.value == nil && rhs.value == nil {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
-extension HSJsonValue: Codable {
+extension HSJsonObj: Codable {
     public init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
         if let v = try? container.decode(Int.self) {
@@ -62,9 +78,9 @@ extension HSJsonValue: Codable {
             value = v
         } else if let v = try? container.decode(Bool.self) {
             value = v
-        } else if let v = try? container.decode([HSJsonValue].self) {
+        } else if let v = try? container.decode([HSJsonObj].self) {
             value = v
-        } else if let v = try? container.decode([String:HSJsonValue].self) {
+        } else if let v = try? container.decode([String:HSJsonObj].self) {
             value = v
         }
     }
@@ -79,9 +95,9 @@ extension HSJsonValue: Codable {
             try container.encode(v)
         } else if let v = value as? Bool {
             try container.encode(v)
-        } else if let v = value as? [HSJsonValue] {
+        } else if let v = value as? [HSJsonObj] {
             try container.encode(v)
-        } else if let v = value as? [String:HSJsonValue] {
+        } else if let v = value as? [String:HSJsonObj] {
             try container.encode(v)
         } else {
             try container.encodeNil()
--- Sources/HoshiJsonMacroClient/main.swift
+++ Sources/HoshiJsonMacroClient/main.swift
@@ -2,10 +2,10 @@ import Foundation
 import HoshiJsonMacro
 
 @HoshiJson class ClassTestD {
-    var bizExtraData: [String:HSJsonValue] = [:]
+    var bizExtraData: [String:HSJsonObj] = [:]
 }
 
-let d = ClassTestD(dict: [
+let d1 = ClassTestD(dict: [
     "biz_extra_data": [
         "int": 1,
         "double": 2.1,
@@ -16,4 +16,15 @@ let d = ClassTestD(dict: [
         "dict": ["int": 4, "double": 5.1, "str": "def", "bool": false, "nil": nil, "array": [4, 5, 6]],
     ]
 ])
-print(d.jsonString)
+let d2 = ClassTestD(dict: [
+    "biz_extra_data": [
+        "int": 1,
+        "double": 2.1,
+        "str": "abc",
+        "bool": true,
+        "nil": nil,
+        "array": [1, 2, 3],
+        "dict": ["int": 4, "double": 5.2, "str": "def", "bool": false, "nil": nil, "array": [4, 5, 6]],
+    ]
+])
+print(d1 == d2)
